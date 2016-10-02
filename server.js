3282080dd5cbc79c92083aa16ae4e1fdba3bb19b
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-10-01 20:56:20 -0700

    Dummy site mostly complete for testing with my A/B idea

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var port = process.env.PORT || 3000;
+
+// a convenient variable to refer to the HTML directory
+var public = './public/';
+
+// routes to serve the static HTML files individually
+app.get('/landingPage', function(req, res) {
+    res.sendfile(public + 'landingPage.html');
+});
+app.get('/thankYouPage', function(req, res) {
+    res.sendfile(public + 'thankYouPage.html');
+});
+
+
+
+
+/* For Serving up HTML &
+
+app.use('/public', express.static(path.join(__dirname + '/public')));
+
+app.get('/public/landingPage', function(req, res) {
+  console.log(`server side: inside of ${landingPage}`)
+  res.sendfile(public + 'landingPage.html');
+});
+
+app.get('/public/thankYouPage', function(req, res) {
+  console.log(`server side: inside of ${thankYouPage}`)
+  res.sendfile(public + 'thankYouPage.html');
+});
+
+*/
+
+
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
